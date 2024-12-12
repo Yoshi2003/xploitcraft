84cb6207fe79880dca2ed57603790ca8b64ca58b
Yoshi2003 <cartertechsolution@gmail.com>
2024-12-12 14:22:22 -0500

    Delete log_history.py

--- log_history.py
+++
@@ -1,8 +0,0 @@
-# models/log_history.py
-from datetime import datetime
-from pydantic import BaseModel
-
-class LogHistory(BaseModel):
-    log: dict  # Entire log structure
-    analysis: str
-    timestamp: datetime
