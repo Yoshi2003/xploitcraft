b67bd91cace3baff2336ebc5be3aae1f9e2b0fd3
Yoshi2003 <cartertechsolution@gmail.com>
2024-12-12 14:38:38 -0500

    Update App.js

--- frontend/my-react-app/src/App.js
+++ frontend/my-react-app/src/App.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from './components/Sidebar/Sidebar';
 
-import Home from './components/pages/HomePage/Home';
+import Xploitcraft from './components/pages/XploitcraftPage/Xploitcraft';
 import About from './components/pages/AboutPage/About'; // About component
 import ScenarioSphere from './components/pages/ScenarioPage/ScenarioSphere';
 import AnalogyHub from './components/pages/AnalogyPage/AnalogyHub';
-import Loganal from './components/pages/LogPage/Loganal';
+import Log from './components/pages/LogPage/Log';
 import GRC from './components/pages/GRCpage/GRC';
 import DailyCyberBrief from './components/pages/DailyPage/DailyCyberBrief';
 import Resources from './components/pages/ResourcesPage/Resources';
 import Donate from './components/pages/DonatePage/Donate';
 
-import './components/pages/HomePage/App.css';
+import './components/pages/XploitcraftPage/App.css';
 
 function App() {
   return (
@@ -22,7 +22,7 @@ function App() {
         <div className="main-content">
           <Routes>
             <Route path="/about" element={<About />} />
-            <Route path="/home" element={<Home />} />
+            <Route path="/xploitcraft" element={<Xploitcraft />} />
             <Route path="/scenariosphere" element={<ScenarioSphere />} />
             <Route path="/analogyhub" element={<AnalogyHub />} />
             <Route path="/loganal" element={<Loganal />} />
